7c9b1ba4a03955a30b66bf5489a2f8adf589a22b
Bekah Tangney <btangney@sparkcognition.com>
2021-12-03 20:36:31 -0600

    Completed day 2

---
+++ Day2/2-1.py
@@ -0,0 +1,22 @@
+def main():
+
+    instructions = []
+
+    with open('input.txt') as file:
+        instructions = [ line.strip() for line in file ]
+
+    horizontal = 0
+    depth = 0
+
+    for instruction in instructions:
+        if instruction[0] == 'f':
+            horizontal += int(instruction[-1])
+        elif instruction[0] == 'u':
+            depth -= int(instruction[-1])
+        elif instruction[0] == 'd':
+            depth += int(instruction[-1])
+
+    total = horizontal * depth
+    print(total)
+    
+main()
