ca8e694156a811e747571cf7ef297b43c692d120
Bekah Tangney <btangney@sparkcognition.com>
2021-12-11 09:02:17 -0600

    Finally completed day 4 part 1... huff, wheeze

--- Day4/4-1.py
+++ Day4/4-1.py
@@ -1,33 +1,41 @@
 def main():
-
     bingo_order = []
     bingo_data = []
+    bingo_card_list = []
     bingo_cards = []
 
     with open('input.txt') as file:
         # read in the first line as the bingo order
         bingo_order = file.readline().strip()
-
         # create a 2nd list of all the bingo card data, just line by line for now
-        for line in file:
-            if line.strip():
-                bingo_data.append(list(map(int, file.readline().split())))
-    
-    # now convert bingo_order into a proper list (as currently it's just a string of numbers and commas)
+        bingo_data = file.read().splitlines()
+
+    # convert bingo_order into a proper list (as currently it's just a string of numbers and commas)
     bingo_order = [int(num) for num in bingo_order.split(',')]
 
-    # now remove the blank lines (that turned into empty lists []) in bingo_data
-    bingo_data = [item for item in bingo_data if item != []]
+    # remove the blank lines (that turned into empty lists []) in bingo_data, and the leading whitespace
+    bingo_data = [item.lstrip() for item in bingo_data if item != '']
+
+    # go through each list of space-separated numbers in the original list and split them to a list of bingo cards
+    # then convert each to an int instead of a string
+    for item in bingo_data:
+        bingo_card_list.append(item.split())
+    bingo_card_list = [[int(num) for num in line] for line in bingo_card_list]
 
     # now, go through all the bingo card data and turn each 5 lines into a bingo card 
     # (the end result is a list of 5x5 matrices)
-    while bingo_data:
-        bingo_cards.append(bingo_data[0:5])
-        del bingo_data[0:5]
-    
-    print(bingo_order)
-    #print(bingo_cards)
+    while bingo_card_list:
+       bingo_cards.append(bingo_card_list[0:5])
+       del bingo_card_list[0:5]
 
+    winning_bingo_card = []
+
+    print("Let's play bingo!")
+    print()
+    print("......")
+    print()
+
+    is_winning = False
     # for each number in the bingo_order list...
     for number in bingo_order:
         # check each bingo card in bingo_cards...
@@ -36,30 +44,65 @@ def main():
             for j, line in enumerate(bingo_card):
                 # ... to see if the number called matches a number on any of the lines
                 for k, item in enumerate(line):
-                    if number in line:
+                    if line[k] == number:
                         bingo_cards[i][j][k] = 'X'
 
                         is_winning = win_condition(bingo_card)
-                        print("Is winning?", is_winning)
+                        if is_winning == True:
+                            print("the following bingo card won!")
+                            winning_bingo_card = bingo_card
+                            print(print_bingo_card(winning_bingo_card))
+                            print("winning bingo number:", number)
+                            winning_bingo_number = number
+                            break
+                    if is_winning == True:
+                        break
+                if is_winning == True:
+                    break
+            if is_winning == True:
+                break
+        if is_winning == True:
+            break
 
-    print(bingo_cards)
-    print(bingo_order)
+    # Calculate the sum of non-marked squares on the winning bingo card
+    sum = 0
+    for x in range(0, 5):
+        for y in range(0, 5):
+            if bingo_card[x][y] != 'X':
+                sum += bingo_card[x][y]
+    
+    final_score = sum * winning_bingo_number
+    print("sum of non-marked items on the winning bingo card:", sum)
+    print("final score:", final_score)
 
-main()
 
-def win_condition(bingo_card[]):
+def print_bingo_card(bingo_card):
+    for x in range(0, 5):
+        print(bingo_card[x])
+
+
+def win_condition(bingo_card):
     is_winning = False
 
-    horizontal_line = []
-    vertical_line = []
-
+    # Checking to see if a horizontal line has hit bingo
     for x in range(0, 5):
+
+        horizontal_line = []
         for y in range(0, 5):
             horizontal_line.append(bingo_card[x][y])
         if horizontal_line == ['X', 'X', 'X', 'X', 'X']:
             is_winning = True
     
-    for 
-    
+    # Checking to see if a vertical line has hit bingo
+    for y in range(0, 5):
+
+        vertical_line = []
+        for x in range(0, 5):
+            vertical_line.append(bingo_card[x][y])
+        if vertical_line == ['X', 'X', 'X', 'X', 'X']:
+            is_winning = True
 
     return is_winning
+
+
+main()
